5429d43d3a18a1d9a9153e689f23b656eb502dc7
calero <zecarloscapcha@gmail.com>
2024-09-26 06:40:14 -0500

    Segunda commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,23 +3,25 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <p class="text-red-500">Bienvenido a la página de categorías.</p>
+
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <!-- CUADRADO BLANCO -->
+
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
-                <div>
-                    <div class="container mx-auto">
-                        <a href="{{ route('categorias') }}" class="block w-full bg-blue-600 text-black uppercase rounded hover:bg-blue-500 py-2 text-center">
-                            Ir a Categorías
-                        </a>
-                    </div>
 
-
+                <div class="container mx-auto">
+                    <a href="{{ route('categorias') }}" class="block text-blue-500 uppercase rounded py-2 text-center">
+                        Ir a Categorías
+                    </a>
                 </div>
+
             </div>
         </div>
     </div>
